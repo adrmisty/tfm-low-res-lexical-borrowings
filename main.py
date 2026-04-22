966dc295d2f4a831e974afa7f8dd02a77a4929d1
adrmisty <adrirflorez@gmail.com>
2026-04-22 13:02:24 +0200

    CLI support and new params for experiments

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # main.py
 # ----------------------------------------------------------------
-# lexical borrowing identification + classification pipeline
+# lexical borrowing identification pipeline
 # ----------------------------------------------------------------
 # adriana r.f. (@adrmisty)
-# apr-2026
+# mar-2026
 
 import os
 import argparse
@@ -13,83 +13,48 @@ from src.model.baseline.eval import evaluate_pipeline
 
 logging.basicConfig(level=logging.INFO, format="INFO: %(message)s")
 
-GOLD_STD_PATH = "data/corpus/test_gold_annotations.json"
+GOLD_STD_PATH = "data/annotation/test_gold_annotations.json"
 SILVER_STD_PATH = "data/corpus/processed/mined_sentences.clean.jsonl"
 
 def main():
     parser = argparse.ArgumentParser(description="[TFM] Lexical borrowing detection pipeline")
     parser.add_argument("--action", type=str, choices=["run", "eval"], default="run", help="Choose to run a model or evaluate predictions")
-    parser.add_argument("--type", type=str, choices=["llm", "langid", "encoder"], default="llm")
-    
-    # ** extend experiments: run 1step/2step, k-shots, different languages **
-    parser.add_argument("--pipeline", type=str, choices=["1step", "2step"], default="2step", help="Architecture to run")
-    parser.add_argument("--k", type=int, default=0, help="Number of few-shot examples to inject per language")
-    parser.add_argument("--langs", nargs="+", default=["ast", "eu", "el"], help="List of languages to process")
-    
+    parser.add_argument("--type", type=str, choices=["llm", "langid", "xlmr"], default="llm")
+    parser.add_argument("--model", type=str, default="Qwen/Qwen3.5-9B")
+    parser.add_argument("--langs", nargs="+", default=["ast", "eu", "el"])
     parser.add_argument("--pred_file", type=str, help="Path to prediction JSON (required if --action=eval)")
-    parser.add_argument("--title", type=str, default="EXPERIMENT", help="Title for the evaluation plots")
+    parser.add_argument("--title", type=str, default="EXPERIMENT", help="Title for the evaluation plots (e.g., QWEN-FEW-SHOT)")
     args = parser.parse_args()
 
     if not os.path.exists(GOLD_STD_PATH):
         logging.error(f"(!) Gold standard file not found at: {GOLD_STD_PATH}")
         return
 
-    # ** evaluation: joint + split steps & language, confusion matrices, metrics **
+    # --- eval pipeline (2-step) ---
     if args.action == "eval":
         if not args.pred_file or not os.path.exists(args.pred_file):
-            logging.error("\t> (!) Warning: provide a valid path to a JSON file using --pred_file")
+            logging.error("> (!) Please provide a valid path to a JSON file using --pred_file")
             return
             
         out_dir = os.path.dirname(args.pred_file)
-        
-        # joint language evaluation
-        if len(args.langs) > 1:
-            logging.info(f">> Evaluation for: {args.langs}")
-            evaluate_pipeline(
-                pred_path=args.pred_file, 
-                gold_path=GOLD_STD_PATH, 
-                out_dir=out_dir, 
-                experiment=f"{args.title}_JOINT",
-                target_langs=args.langs # Pass the filter!
-            )
-        
-        # single language evaluation
-        for lang in args.langs:
-            logging.info(f">> Evaluation for: {lang.upper()}")
-            evaluate_pipeline(
-                pred_path=args.pred_file, 
-                gold_path=GOLD_STD_PATH, 
-                out_dir=out_dir, 
-                experiment=f"{args.title}_{lang.upper()}",
-                target_langs=[lang]
-            )
+        evaluate_pipeline(
+            pred_path=args.pred_file, 
+            gold_path=GOLD_STD_PATH, 
+            out_dir=out_dir, 
+            experiment=args.title
+        )
         return
 
-    # *** baseline runs: LLM prompting, language identification, XLM-RoBERTa ***
+    # --- exec inference pipeline ---
     if args.type == "langid":
         run_langid_baseline(langs=args.langs, gt=GOLD_STD_PATH)
-            
     elif args.type == "llm":
-        """python main.py --action run --type llm --pipeline 2step --k 2 --langs ast eu el --model Qwen/Qwen3.5-9B
-        python main.py --action eval --pred_file results/model/Qwen/Qwen3.5-9B/predictions_Qwen-Qwen3.5-9B_{date}.json --title QWEN3.5_2SH"""
-        run_llm_baseline(
-            langs=args.langs, 
-            model_id=args.model, 
-            gt=GOLD_STD_PATH, 
-            pipeline=args.pipeline, 
-            k=args.k
-        )
-        
+        run_llm_baseline(langs=args.langs, model_id=args.model, gt=GOLD_STD_PATH)
     elif args.type == "xlmr":
         if not os.path.exists(SILVER_STD_PATH):
-            logging.error(f"\t> (!) Silver data file for multilingual encoder not found at: {SILVER_STD_PATH}")
+            logging.error(f"> (!) Silver data file not found at: {SILVER_STD_PATH}")
             return
-        run_xlmr_baseline(
-            langs=args.langs, 
-            silver_data=SILVER_STD_PATH, 
-            gt=GOLD_STD_PATH,
-            pipeline=args.pipeline
-        )
+        run_xlmr_baseline(langs=args.langs, silver_data=SILVER_STD_PATH, gt=GOLD_STD_PATH)
 
 if __name__ == "__main__":
     main()
